abcc274c0b89c621ac106198ae97e0cfc6321f5a
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2015-10-29 16:51:28 +0100

    [NEW] finished exercice chain lesson2

--- lesson2/exercices/guillaume/chain.js
+++ lesson2/exercices/guillaume/chain.js
@@ -1,17 +1,21 @@
 function loopSize(node){
 
-  var the_node = node;
-  var i = 0;
+  var the_node = node,
+    i = 0,
+    tab = [];
+  
+  // On boucle en vérifiant si le tableau ne contient pas le noeud
+  while(tab.indexOf(the_node) == -1) {
+    // Si non, on ajoute le noeud au tableau
+    tab[i] = the_node;
 
-  //console.log(the_node);
-
-  while(typeof node.next != 'undefined' && i < 10) {
-    //console.log(the_node);
-    the_node = node.next;
+    // On passe au noeud suivant
+    the_node = the_node.next;
     i = i + 1;
   }
 
-  return i;
+  // On retourne le nombre de fois où on a bouclé moins l'offset du noeud retrouvé pour renvoyer la taille de la loop
+  return i - tab.indexOf(the_node);
 }
 
 // Testing environment
@@ -19,14 +23,11 @@ function createChain(tailLength, loopLength){
   list = [];
   for(i=0; i < tailLength + loopLength; i++){
     list.push({ next: null });
-    console.log('1', list);
     if(i > 0){
       list[i-1].next = list[i];
     }
   }
-  console.log(list);
   list[tailLength + loopLength - 1].next = list[tailLength];
-  console.log(list[0]);
 
   return list[0];
 }
@@ -40,33 +41,33 @@ if(result != 1){
   console.log("Test 1: success");
 }
 
-// Test 2
-// list = createChain(8778, 23);
-// result = loopSize(list)
-// if(result != 23){
-//   console.log("Test 2: Expected 23 got " + result)
-// } else {
-//   console.log("Test 2: success");
-// }
+//Test 2
+list = createChain(8778, 23);
+result = loopSize(list)
+if(result != 23){
+  console.log("Test 2: Expected 23 got " + result)
+} else {
+  console.log("Test 2: success");
+}
 
-// // Test 3
-// list = createChain(23, 8778);
-// result = loopSize(list)
-// if(result != 8778){
-//   console.log("Test 3: Expected 8778 got " + result)
-// } else {
-//   console.log("Test 3: success");
-// }
+// Test 3
+list = createChain(23, 8778);
+result = loopSize(list)
+if(result != 8778){
+  console.log("Test 3: Expected 8778 got " + result)
+} else {
+  console.log("Test 3: success");
+}
 
-// // Test 4
-// for(i = 0 ; i < 2 ; i ++){
-//   var a = Math.floor(Math.random()*999) + 9000;
-//   var b = Math.floor(Math.random()*999) + 9000;
-//   list = createChain(a, b);
-//   result = loopSize(list)
-//   if(result != b){
-//     console.log("Test 4: Expected " + b + " got " + result)
-//   } else {
-//     console.log("Test 4: success");
-//   }
-// } 
+// Test 4
+for(i = 0 ; i < 2 ; i ++){
+  var a = Math.floor(Math.random()*999) + 9000;
+  var b = Math.floor(Math.random()*999) + 9000;
+  list = createChain(a, b);
+  result = loopSize(list)
+  if(result != b){
+    console.log("Test 4: Expected " + b + " got " + result)
+  } else {
+    console.log("Test 4: success");
+  }
+} 
