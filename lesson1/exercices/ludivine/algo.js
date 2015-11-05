97368e3220cbd33f5fefc7422f4722939e3a0441
ECV Digital 03 <Ecvdigital03@ECV-PA-D001-IM003.local>
2015-11-05 13:10:11 +0100

    Exercice Loop

--- lesson1/exercices/ludivine/algo.js
+++ lesson1/exercices/ludivine/algo.js
@@ -1,15 +1,15 @@
 // Time complexity of code
 
 // Ex 1
-for( var i = n; i > 0; i /= 2 ) {
-  for( var j = 1; j < n; j *= 2 ) {
-    for( var k = 0; k < n; k += 2 ) {
+for( var i = n; i > 0; i /= 2 ) { // n, n/2, n/4, n/8, ... log(n)
+  for( var j = 1; j < n; j *= 2 ) { // 1, 2, 4, 8, 16, 32 log(n)
+    for( var k = 0; k < n; k += 2 ) { // 0, 2, 4, 6, 8 n
       ... // constant number of operations
     }
   }
 }
 /*
-Answer:
+Answer: all function are not independent so : n log^2(n)
 */
 
 
@@ -22,14 +22,14 @@ for(var i = 1; i < n; i *= 2 ) {
   }
 }
 /*
-Answer:
+Answer: Same as first one 
 */
 
 
 // Ex3
-for(var i = n; i > 0; i-- ) {
-  for(var j = 1; j < n; j *= 2 ) {
-    for(var k = 0; k < j; k++ ) {
+for(var i = n; i > 0; i-- ) { // n 
+  for(var j = 1; j < n; j *= 2 ) { // log(n)
+    for(var k = 0; k < j; k++ ) { // j = 1, 2, 4, 8... 
       ... // constant number C of operations
     }
   }
