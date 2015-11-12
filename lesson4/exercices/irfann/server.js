f8c36a9341b3ba343ccee1ff185c74ad5e164ca5
ECV Digital 02 <Ecvdigital02@ECV-PA-D001-IM002-2.local>
2015-11-12 16:19:43 +0100

    Proxy

--- lesson4/exercices/irfann/server.js
+++ lesson4/exercices/irfann/server.js
@@ -3,6 +3,36 @@ var http = require('http');
 var net = require('net');
 var url = require('url');
 
+
+var getAll = function (location, res){
+  var options = {
+      host: location
+    };
+
+  http.request(options, function(resp) {
+      var data ='';
+      resp.on('data', function(chunk){
+          data += chunk;
+      });
+      resp.on('end', function() {
+        if (resp.statusCode === 302 || resp.statusCode === 301 && resp.headers.location) {
+          var url = resp.headers.location;
+          var protomatch = /.*?:\/\/www./g;
+          var b = url.replace(protomatch, '');
+          console.log(b);
+          getAll(url, res);
+        }else{
+          res.writeHead(resp.statusCode, resp.headers);
+          res.write(data);
+          res.end();
+        }
+      });
+    }).on('error', function(e) {
+      console.log("Got error: " + e.message);
+
+    }).end();
+}
+
 // We create the server
 http.createServer(function (req, res) { // This callback is called every time a request come to the webserver
     if(/favicon/i.test(req.url)){
@@ -14,25 +44,15 @@ http.createServer(function (req, res) { // This callback is called every time a 
     var args = url[1];
     var ar = args.split("&");
     var paramObjet = {};
+    console.log(args);
+
 
     for(var i = 0; i < ar.length; i++){
       var argument = ar[i].split('=');
       paramObjet[argument[0]] = argument[1];
     }
-
-    var options = {
- 
-      hostname: paramObjet.site
-
-    };
-
-    http.get(options, function(resp) {
-      resp.on('data', function(chunk){
-        res.end(chunk);
-      });
-    }).on('error', function(e) {
-      console.log("Got error: " + e.message);
-    });
+    
+    getAll(paramObjet.site, res);
 
 
 }).listen(1337, "127.0.0.1");
